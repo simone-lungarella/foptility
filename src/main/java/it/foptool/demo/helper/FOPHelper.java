e80ec725453e05035a82ba688d0e4662d1238764
Simone <simonelungarella@gmail.com>
2021-12-02 14:43:55 +0100

    Implemented helper and test class to generate pdf's

---
+++ src/main/java/it/foptool/demo/helper/FOPHelper.java
@@ -0,0 +1,98 @@
+package it.foptool.demo.helper;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.text.StringEscapeUtils;
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Helper FOP.
+ * 
+ * @author Simone Lungarella
+ */
+@Slf4j
+public final class FOPHelper {
+
+    /**
+     * Transformer Factory.
+     */
+    private static TransformerFactory tFactory = TransformerFactory.newInstance();
+
+    /**
+     * Fop Factory.
+     */
+    private static FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+
+    /**
+     * Empty constructor.
+     */
+    private FOPHelper() {
+        // Constructor that hides the public and default constructor.
+    }
+
+    /**
+     * Transforms into pdf the {@code xslt} file populating its parameter using
+     * values in the map: {@code hm}.
+     * 
+     * @param params Map of parameters to use to populate the xslt file.
+     * @param xslt   Template fop to generate the pdf from.
+     * @return Byte array of pdf generated.
+     */
+    public static byte[] transformMAP2PDF(final Map<String, String> params, final byte[] xslt) {
+        try {
+            return transformSRC2PDF(getXMLSourceFromMAP(params), xslt);
+        } catch (FOPException | IOException | TransformerException e) {
+            log.info("Error while generating pdf");
+        }
+        return new byte[0];
+    }
+
+    private static Source getXMLSourceFromMAP(final Map<String, String> hm) {
+        final StringBuilder result = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
+        result.append("<parameters>");
+        for (final Entry<String, String> e : hm.entrySet()) {
+            result.append("<" + e.getKey() + ">");
+            result.append(StringEscapeUtils.escapeXml10(e.getValue()));
+            result.append("</" + e.getKey() + ">");
+        }
+        result.append("</parameters>");
+        final String str = result.toString();
+        log.info("Marshalling per FOP: " + str);
+        return new StreamSource(new StringReader(str));
+    }
+
+    private static byte[] transformSRC2PDF(final Source srcXML, final byte[] xslt)
+            throws FOPException, IOException, TransformerException {
+        final InputStream streamXSLT = new ByteArrayInputStream(xslt);
+        final Source srcXSLT = new StreamSource(streamXSLT);
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
+        final Result res = new SAXResult(fop.getDefaultHandler());
+        final Transformer transformer = tFactory.newTransformer(srcXSLT);
+        transformer.transform(srcXML, res);
+        out.flush();
+        out.close();
+        return out.toByteArray();
+    }
+
+}
---
+++ src/test/java/it/foptool/demo/PDFCreatorTest.java
@@ -0,0 +1,105 @@
+package it.foptool.demo;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import it.foptool.demo.helper.FOPHelper;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * PDF Creator test class.
+ * 
+ * @author Simone Lungarella
+ */
+@Slf4j
+@SpringBootTest
+class PDFCreatorTest {
+
+	/**
+	 * Author placeholder on xslt file.
+	 */
+	private final String AUTHOR_PH = "author";
+
+	/**
+	 * Date placeholder on xslt file.
+	 */
+	private final String DATE_PH = "today-date";
+
+	/**
+	 * Notes placeholder on xslt file.
+	 */
+	private final String NOTES_PH = "notes";
+
+	/**
+	 * Header placeholder on xslt file.
+	 */
+	private final String HEADER_PH = "header";
+
+	@Test
+	void xsltToPdf() {
+		final String xsltFilename = "xsltExample.xslt";
+		final String pdfFilename = "pdfExample.pdf";
+
+		log.info("Recupero file");
+		byte[] xsltFile = getFileFromResources(xsltFilename);
+		assertNotNull(xsltFile, "Xslt file not retrieved correctly");
+
+		log.info("Params definition");
+		final String author = "Simone Lungarella";
+		final String header = "XSLT Template header of example";
+		final String notes = "Random text injected from code";
+		final String today_date = new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+
+		Map<String, String> params = new HashMap<>();
+		params.put(AUTHOR_PH, author);
+		params.put(DATE_PH, today_date);
+		params.put(NOTES_PH, notes);
+		params.put(HEADER_PH, header);
+
+		log.info("Generating PDF");
+
+		byte[] pdfFile = FOPHelper.transformMAP2PDF(params, xsltFile);
+		assertNotNull(pdfFile, "PDF file not generated correctly");
+
+		saveFileToResources(pdfFile, pdfFilename);
+		log.info("Transformation completed, pdf file generated in resource directory");
+
+	}
+
+	private byte[] getFileFromResources(String filename) {
+		String filepath = this.getClass().getResource("/").getPath().replace("target/test-classes/",
+				"src/test/java/it/foptool/demo/resources/");
+
+		try (InputStream is = new FileInputStream(new File(filepath + filename));) {
+			return is.readAllBytes();
+		} catch (Exception e) {
+			log.error("Error while reading file from internal resources", e);
+		}
+		return null;
+	}
+
+	private void saveFileToResources(byte[] content, String filename) {
+
+		String filepath = this.getClass().getResource("/").getPath().replace("target/test-classes/",
+				"src/test/java/it/foptool/demo/resources/");
+
+		try (BufferedOutputStream bs = new BufferedOutputStream(new FileOutputStream(new File(filepath + filename)))) {
+			bs.write(content);
+		} catch (Exception e) {
+			log.error("Error while saving content on internal resources", e);
+		}
+	}
+
+}
